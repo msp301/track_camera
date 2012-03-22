44e9ba18df2a86c74bcf2a9ed05e00260adb08ec
Martin Pritchard <ece80427@myport.ac.uk>
2012-03-22 15:46:38 +0000

    Added camera input read test
    - Added functionality directly into main function to read input
      from connected USB camera and display in a window
    - This only uses the supplied functionality available through
      OpenCV's HighGui class and does not use windowing directly
      through Qt

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <QtGui/QApplication>
 #include "GuiMainWindow.hpp"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	GuiMainWindow w;
-	w.show();
-	
-	return a.exec();
+    QApplication a(argc, argv);
+    //GuiMainWindow w;
+    //w.show();
+
+    cv::VideoCapture capture( 0 ); //create camera interface
+    cv::namedWindow( "window", 1 ); //create new UI window ready for display
+
+    //check that camrea is ready before attempting to read
+    if( capture.isOpened() )
+    {
+        //constantly sample video input and display frames to window
+        for( ;; )
+        {
+            cv::Mat frame;
+            capture >> frame; //read next frame from video capture
+
+            cv::imshow( "window", frame ); //display frame in window
+            if( cv::waitKey( 20 ) >= 0 ) break; //check if user wants to exit
+        }
+    }
+    capture.release(); //disconnect camera before closing program
+
+    return 0;
+    //return a.exec();
 }
