1f34a6d8b58856a231e0fe1dd809e73a5fd86d33
Martin Pritchard <ece80427@myport.ac.uk>
2012-03-26 11:19:06 +0100

    Cleaned unused code from GUI
    - Removed commented code that is not used to display output of
      camera

--- GuiMain.cpp
+++ GuiMain.cpp
@@ -9,10 +9,6 @@ GuiMain::GuiMain( string name, Capture *camera )
     input = camera;
     window_name = name /*"Track Camera"*/;
     cv::namedWindow( window_name, 1 );
-
-    //typedef void ( *CLOSE ) ( void );
-    //CLOSE callbackClose = close();
-    //cv::createButton( "btn_close", ( this->close() ) );
 }
 
 GuiMain::~GuiMain()
@@ -23,15 +19,12 @@ GuiMain::~GuiMain()
 
 void GuiMain::run()
 {
-    while( /*int i=0; i<50; i++*/ true )
+    while( true )
     {
-        //std::cout << "i = " << i << endl;
         cv::Mat frame = input->grabFrame();
         cv::imshow( window_name, frame );
         cv::waitKey( 20 );
     }
-    //cv::destroyAllWindows();
-    this->quit();
 }
 
 void GuiMain::close()
--- main.cpp
+++ main.cpp
@@ -3,37 +3,26 @@
 #include "Capture.hpp"
 #include "GuiMain.hpp"
 
+#include <iostream>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 int main(int argc, char *argv[])
 {
-    //QApplication a(argc, argv);
-    //GuiMainWindow w;
-    //w.show();
-
-    //cv::namedWindow( "window", 1 ); //create new UI window ready for display
-
     Capture *camera = new Capture; //create new camera object
     GuiMain *window = new GuiMain( "window", camera );
     window->start(); //start window
 
-    //window->displayVideo( camera );
-
-    //cv::imshow( "window", frame ); //display frame in window
-
-    while( true );
-
-    //while( true )
-    //{
-    //    cv::Mat frame = camera->grabFrame();
-    //    imshow( "window", frame );
-    //    if( cv::waitKey( 20 ) >= 0 ) break; //check if user wants to exit
-    //}
+    while( true )
+    {
+        //char key;
+        //std::cin >> key;
+        //if( key  == 'q' ) break;
+    }
 
     delete camera; //disconnect camera before closing program
     delete window;
 
     return 0;
-    //return a.exec();
 }
