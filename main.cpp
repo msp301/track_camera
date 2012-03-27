bbab198e2e274ca252434fa23b28cbc8c9f321b5
Martin Pritchard <ece80427@myport.ac.uk>
2012-03-27 14:33:06 +0100

    Removed process control from main function
    - Elements are now being moved back to a proper Qt application and
      so all implementation will now be started from UI interactions
      that will be handled by the application window
    - This now means that all the main function requires to do is start
      the initial application and not be directly concerned with the
      application's operations

--- main.cpp
+++ main.cpp
@@ -1,36 +1,11 @@
 #include <QtGui/QApplication>
 #include "GuiMainWindow.hpp"
-#include "Capture.hpp"
-#include "GuiMain.hpp"
-#include "FaceTracking.hpp"
-
-#include <iostream>
-#include <vector>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
 int main(int argc, char *argv[])
 {
-    VideoBuffer *buffer = new VideoBuffer; //create new video buffer
-    Capture *camera = new Capture( buffer ); //create new camera object
-    camera->start(); //start capturing video stream from camera
+    QApplication app( argc, argv ); //create new Qt application container
+    GuiMainWindow window; //create main window
+    window.show(); //display main window
 
-    GuiMain *window = new GuiMain( "window", buffer );
-    window->start(); //start window
-
-    //FaceTracking *tracker = new FaceTracking( camera );
-    //tracker->start();
-
-    while( true )
-    {
-        //char key;
-        //std::cin >> key;
-        //if( key  == 'q' ) break;
-    }
-
-    delete camera; //disconnect camera before closing program
-    delete window;
-
-    return 0;
+    return app.exec(); //start application
 }
