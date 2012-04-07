965a1516565373fdc0c0d628bb312ff6259dc8fb
Martin Pritchard <ece80427@myport.ac.uk>
2012-04-07 23:17:50 +0100

    Reduced processing load when detecting faces
    - Reduced CPU load when processing video frames to detect faces
      by reducing the frame image size before continuing to process
      the image
    - Video frame is reduced to half the size of the original and
      the detected facial areas are scaled to the size of the output
      image so that displayed faces in the output video are correctly
      drawn on the original frame

--- FaceTracking.cpp
+++ FaceTracking.cpp
@@ -47,6 +47,7 @@ void FaceTracking::run()
             else
             {
                 qDebug() << "Frame Discarded";
+                if( showDetectedFaces() ) displayDetectedFaces( frame, faces );
             }
         }
     }
@@ -59,7 +60,10 @@ vector<cv::Rect> FaceTracking::detectFace( cv::Mat frame )
     cv::CascadeClassifier haar_face_classifier;
     vector<cv::Rect> faces;
 
-    cv::cvtColor( frame, conv_frame, CV_BGR2GRAY ); //convert to greyscale
+    //scale image to reduce processing load
+    cv::resize( frame, conv_frame, cv::Size(), 0.5, 0.5, CV_INTER_NN );
+
+    cv::cvtColor( conv_frame, conv_frame, CV_BGR2GRAY ); //convert to greyscale
     cv::equalizeHist( conv_frame, conv_frame );
 
     //load face classifier
@@ -67,7 +71,7 @@ vector<cv::Rect> FaceTracking::detectFace( cv::Mat frame )
     {
         //match faces of different sizes within video frame
         haar_face_classifier.detectMultiScale( conv_frame, faces, 1.1, 2,
-                                0 | CV_HAAR_SCALE_IMAGE, cv::Size( 30, 30 ) );
+                                0 | CV_HAAR_SCALE_IMAGE, cv::Size( 5, 5 ) );
 
         qDebug() << "Faces detected = " << faces.size();
     }
@@ -81,16 +85,17 @@ void FaceTracking::displayDetectedFaces( cv::Mat frame, vector<cv::Rect> faces )
     //loop through each detected face and draw a rectangle around it
     foreach( cv::Rect face, faces )
     {
-        cv::Point pt1( face.x, face.y ); //top-left position of face area
+        cv::Point pt1( face.x * 2, face.y * 2 ); //top-left position of face area
 
         //calculate bottom-right coordinates of face given its area
-        cv::Point pt2( ( ( face.x + face.width ) - 1 ),
-                       ( ( face.y + face.height ) - 1 ) );
+        cv::Point pt2( ( ( ( ( face.x + face.width ) - 1 ) ) * 2 ),
+                       ( ( ( ( face.y + face.height ) - 1 ) ) * 2 ) );
 
         //draw rectangle on image to specified size of face
         cv::rectangle( frame, pt1, pt2, cv::Scalar( 255, 0, 0, 0 ), 4, 8, 0 );
 
         qDebug() << "FaceTracking: adding detected frame to buffer";
+
         video_buffer->add( frame ); //add processed frame to output buffer
     }
 }
