e306b1ae444091bb93b41dd4b213b2ff83af2b3e
Martin Pritchard <ece80427@myport.ac.uk>
2012-04-23 14:27:02 +0100

    Added HardwareController source to project
    - Added Arduino 'HardwareController' code to project
    - Currently is used to wait for incoming serial data and setting
      an LED if the received data was what the controller expected
    - Arduino source is not built by qmake like the rest of the project
      code, this is done separately by the Arduino IDE and also uploaded
      to the target device

---
+++ arduino/HardwareController/HardwareController.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+void setup()
+{
+	Serial.begin( 9600 ); //set baud rate
+	pinMode( 7, OUTPUT ); //set pin 7 as digital output
+}
+
+void loop()
+{
+	if( Serial.available() )
+	{
+		if( Serial.read() == 'T' )
+		{
+			digitalWrite( 7, HIGH );
+			delay( 10 );
+			digitalWrite( 7, LOW );
+		}
+	}
+}
