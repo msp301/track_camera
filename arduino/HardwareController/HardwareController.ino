926494bf0b2da1652d795e14978b81618e6548d8
Martin Pritchard <ece80427@myport.ac.uk>
2012-04-25 10:28:32 +0100

    First attempt to calculate required stand movements
    - Instructing Arduino to move to required position by using the
      face position identified on the camera image
    - Using the new and previous face positions, could calculate change
      from position 1 to position 2 and then using the percentage
      change on the image to give a change in stand angle to move
    - Arduino does not seem to be capable of combining multiple arithmetic
      into a single expression using doubles, so have had to break down the
      calculations so that they apply properly

--- arduino/HardwareController/HardwareController.ino
+++ arduino/HardwareController/HardwareController.ino
@@ -5,6 +5,11 @@
 Servo servo_x; //define servo to control x-axis movement
 Servo servo_y; //define servo to control y-axis movement
 
+int servo_x_position;
+int servo_y_position;
+int prev_x;
+int prev_y;
+
 //function prototypes
 char* readData( int available_data );
 
@@ -16,6 +21,13 @@ void setup()
 	//setup servo control pins
 	servo_x.attach( 5, 544, 2500 ); //set min & max pulses to ensure 180 degree rotation
 	servo_y.attach( 6 ); //min & max pulses set to defaults (544 & 2400)
+
+	servo_x_position = 90; //set starting angle to centre position
+	servo_x.write( servo_x_position ); //move stand to default position
+
+	//define previous coordinates as null
+	prev_x = NULL;
+	prev_y = NULL;
 }
 
 void loop()
@@ -30,6 +42,62 @@ void loop()
 		//convert received coordinates to integer values
 		int x = coord[0];
 		int y = coord[1];
+
+		Serial.print( "prev_x = " );
+		Serial.println( prev_x, DEC );
+
+		if( prev_x != NULL )
+		{
+			double change;
+			double angle;
+
+			if( x > prev_x )
+			{
+				//change = ( ( x - prev_x ) / 640 ) * 100; //calculate face position change
+				change = x - prev_x;
+				change = change / 640;
+				change = change * 100;
+
+				Serial.print( "Change (Minus) = " );
+				Serial.println( change, DEC );
+
+				//angle = ( 180 / 100 ) * change; //calculate required movement
+				angle = 180 / 100;
+				angle = angle * change;
+
+				Serial.print( "Angle (Minus) = " );
+				Serial.println( angle, DEC );
+
+				servo_x_position = servo_x_position - angle; //update x-axis servo position
+			}
+			else if( x < prev_x )
+			{
+				//change = ( ( x + prev_x ) / 640 ) * 100;
+				change = x + prev_x;
+				change = change / 640;
+				change = change * 100;
+
+				Serial.print( "Change (Plus) = " );
+				Serial.println( change, DEC );
+
+				//angle = ( 180 / 100 ) * change;
+				angle = 180 / 100;
+				angle = angle * change;
+
+				Serial.print( "Angle (Plus) = " );
+				Serial.println( angle, DEC );
+
+				servo_x_position = servo_x_position + angle; //update x-axis servo position
+			}
+
+			servo_x.write( servo_x_position ); //move servo to new position
+			delay( 3000 ); //wait for servo to move to new position
+
+			Serial.print( "Servo position = " );
+			Serial.println( servo_x_position, DEC );
+		}
+
+		prev_x = x; //update just seen coordinate as last seen
 
 		Serial.flush();
 	}
