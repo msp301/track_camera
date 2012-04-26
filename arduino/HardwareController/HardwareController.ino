d6c5f3ce1efd67a4bdbf276031417050fe46a5af
Martin Pritchard <ece80427@myport.ac.uk>
2012-04-26 17:37:02 +0100

    Added y-axis servo control to HardwareController
    - Stand tilting now works if a detected face moves up or down of
      the centre position in the image
    - Tidied HardwareController code, removing unused debug

--- arduino/HardwareController/HardwareController.ino
+++ arduino/HardwareController/HardwareController.ino
@@ -9,6 +9,8 @@ int servo_x_position;
 int servo_y_position;
 int prev_x;
 int prev_y;
+int is_x_centre;
+int is_y_centre;
 
 //function prototypes
 wchar_t* readData( int available_data );
@@ -24,6 +26,9 @@ void setup()
 
 	servo_x_position = 90; //set starting angle to centre position
 	servo_x.write( servo_x_position ); //move stand to default position
+
+	servo_y_position = 80;
+	servo_y.write( servo_y_position );
 
 	//define previous coordinates as null
 	prev_x = NULL;
@@ -43,40 +48,62 @@ void loop()
 		int x = coord[0];
 		int y = coord[1];
 
-		Serial.print( "prev_x = " );
-		Serial.println( prev_x, DEC );
-
 		if( prev_x != NULL )
 		{
 			double change;
 			double angle;
 
-			int is_centre = 1;
+			is_x_centre = 1;
 			if( x > ( 50 + 10 ) )
 			{
 				servo_x_position = servo_x_position + 5; //update x-axis servo position
 				if( servo_x_position > 180 ) servo_x_position = 180; //stop at max position
-				is_centre = 0;
+				is_x_centre = 0;
 			}
 			else if( x < ( 50 - 10 ) )
 			{
 				servo_x_position = servo_x_position - 5; //update x-axis servo position
 				if( servo_x_position < 0 ) servo_x_position = 0; //stop at min position
-				is_centre = 0;
-
+				is_x_centre = 0;
 			}
-
-			if( !is_centre )
-			{
-				servo_x.write( servo_x_position ); //move servo to new position
-				delay( 3 ); //wait for servo to move to new position
-			}
-
-			Serial.print( "Servo position = " );
-			Serial.println( servo_x_position, DEC );
 		}
 
-		prev_x = x; //update just seen coordinate as last seen
+		if( prev_y != NULL )
+		{
+			double change;
+			double angle;
+
+			is_y_centre = 1;
+			if( y > ( 37 + 5 ) )
+			{
+				servo_y_position = servo_y_position - 5; //update y-axis servo position
+				if( servo_y_position < 25 ) servo_y_position = 25; //stop at min position
+				is_y_centre = 0;
+			}
+			else if( y < ( 37 - 5 ) )
+			{
+				servo_y_position = servo_y_position + 5; //update y-axis servo position
+				if( servo_y_position > 100 ) servo_y_position = 100; //stop at max position
+				is_y_centre = 0;
+			}
+		}
+
+		//check whether stand needs to be moved left or right
+		if( !is_x_centre )
+		{
+			servo_x.write( servo_x_position ); //move servo to new position
+			delay( 3 ); //wait for servo to move to new position
+		}
+
+		//check whether stand needs to be moved up or down
+		if( !is_y_centre )
+		{
+			servo_y.write( servo_y_position );
+			delay( 3 );
+		}
+
+		prev_x = x; //update just seen x coordinate as last seen
+		prev_y = y; //update just seen y coordinate as last seen
 
 		Serial.flush();
 	}
@@ -91,8 +118,6 @@ wchar_t* readData( int available_data )
 	for( int i=0; i < available_data; i++ )
 	{
 		data[i] = Serial.read();
-		Serial.print( "Adding " );
-		Serial.println( data[i] );
 	}
 
 	return data;
